851fec56791f39b9df7a63f6eea41117fc2d555a
Will Jones <thefreethinker127@gmail.com>
2016-03-07 10:48:51 -0800

    Add API

---
+++ cache.h
@@ -0,0 +1,32 @@
+#include <inttypes.h>
+
+struct cache_obj;
+typedef struct cache_obj *cache_t;
+
+typedef const uint8_t *key_type;
+typedef const void *val_type;
+
+// For a given key string, return a pseudo-random integer:
+typedef uint64_t (*hash_func)(key_type key);
+
+// Create a new cache object with a given maximum memory capacity.
+cache_t create_cache(uint64_t maxmem);
+
+// Add a <key, value> pair to the cache.
+// If key already exists, it will overwrite the old value.
+// If maxmem capacity is exceeded, sufficient values will be removed
+// from the cache to accomodate the new value.
+void cache_set(cache_t cache, key_type key, val_type val, uint32_t val_size);
+
+// Retrieve the value associated with key in the cache, or NULL if not found.
+// The size of the returned buffer will be assigned to *val_size.
+val_type cache_get(cache_t cache, key_type key, uint32_t *val_size);
+
+// Delete an object from the cache, if it's still there
+void cache_delete(cache_t cache, key_type key);
+
+// Compute the total amount of memory used up by all cache values (not keys)
+uint64_t cache_space_used(cache_t cache);
+
+// Destroy all resource connected to a cache object
+void destroy_cache(cache_t cache);
