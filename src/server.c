04d22548a0b9a513b6da7921eb3f15640d24dacf
Will Jones <thefreethinker127@gmail.com>
2016-05-10 19:48:07 -0700

    Setup all urls

--- src/server.c
+++ src/server.c
@@ -47,15 +47,8 @@ void respond_not_found(evhtp_request_t *req)
     evhtp_send_reply(req, EVHTP_RES_NOTFOUND);
 }
 
-// Test w/: curl -i -X HEAD localhost:8081/4
-void handler_header(evhtp_request_t *req, void *a)
+void add_std_headers(evhtp_request_t *req)
 {
-    // check for correction HTTP request type
-    if (evhtp_request_get_method(req) != htp_method_HEAD) {
-        respond_not_found(req);
-        return;
-    }
-    
     // Add Content-Type
     evhtp_kv_t *header = evhtp_header_new("Content-Type",  "application/json", 0, 1);
     evhtp_headers_add_header(req->headers_out, header);
@@ -73,43 +66,100 @@ void handler_header(evhtp_request_t *req, void *a)
 
     header = evhtp_header_new("Date", time_string, 0, 1);
     evhtp_headers_add_header(req->headers_out, header);
+}
+/*
+  HEAD /k: Just respond with header
+  Test w/: curl -i -X HEAD localhost:8081/4
+*/
+void handler_header(evhtp_request_t *req, void *a)
+{
+    add_std_headers(req);
 
     evhtp_send_reply(req, EVHTP_RES_OK); // Response code is 2nd arg
 }
 
-
-void issue161cb(evhtp_request_t * req, void * a)
+/*
+  GET /k: Get entry with key k
+  Test w/: curl -i -X GET localhost:8081/4
+*/
+void handler_get(evhtp_request_t *req, void *a)
 {
-    struct evbuffer * b = evbuffer_new();
+    add_std_headers(req);
 
-    if (evhtp_request_get_proto(req) == EVHTP_PROTO_10) {
-        evhtp_request_set_keepalive(req, 0);
-    }
-
-    evhtp_send_reply_start(req, EVHTP_RES_OK);
-
-    evbuffer_add(b, "foo", 3);
-    evhtp_send_reply_body(req, b);
-
-    evbuffer_add(b, "bar\n\n", 5);
-    evhtp_send_reply_body(req, b);
-
-    evhtp_send_reply_end(req);
-
-    evbuffer_free(b);
+    evhtp_send_reply(req, EVHTP_RES_OK); // Response code is 2nd arg
 }
 
-int
-main(int argc, char ** argv) {
+/*
+  DELETE /k: Delete entry with key k
+  Test w/: curl -i -X DELETE localhost:8081/4
+*/
+void handler_delete(evhtp_request_t *req, void *a)
+{
+    add_std_headers(req);
+
+    evhtp_send_reply(req, EVHTP_RES_OK); // Response code is 2nd arg
+}
+
+void handler_main(evhtp_request_t *req, void *a)
+{
+   if (evhtp_request_get_method(req) == htp_method_GET)
+       handler_get(req, a);
+   else if (evhtp_request_get_method(req) == htp_method_HEAD)
+       handler_header(req, a);
+   else if (evhtp_request_get_method(req) == htp_method_DELETE)
+       handler_delete(req, a);
+   else
+       respond_not_found(req);
+}
+
+void handler_set(evhtp_request_t *req, void *a)
+{
+    if (evhtp_request_get_method(req) != htp_method_PUT) {
+        respond_not_found(req);
+        return;
+    }
+
+    add_std_headers(req);
+
+    evhtp_send_reply(req, EVHTP_RES_OK); // Response code is 2nd arg    
+}
+
+void handler_shutdown(evhtp_request_t *req, void *a)
+{
+    if (evhtp_request_get_method(req) != htp_method_POST) {
+        respond_not_found(req);
+        return;
+    }
+
+    add_std_headers(req);
+
+    evhtp_send_reply(req, EVHTP_RES_OK); // Response code is 2nd arg    
+}
+
+void handler_setup(evhtp_request_t *req, void *a)
+{
+    if (evhtp_request_get_method(req) != htp_method_POST) {
+        respond_not_found(req);
+        return;
+    }
+
+    add_std_headers(req);
+
+    evhtp_send_reply(req, EVHTP_RES_OK); // Response code is 2nd arg    
+}
+
+
+int main(int argc, char ** argv)
+{
     evbase_t * evbase = event_base_new();
     evhtp_t  * htp    = evhtp_new(evbase, NULL);
 
-    //evhtp_set_cb(htp, "/", handler_greeting, NULL);
-    evhtp_set_regex_cb(htp, "/[0-9]+$", handler_header, NULL);
-    evhtp_set_cb(htp, "/simple/", testcb, "simple");
-    evhtp_set_cb(htp, "/1/ping", testcb, "one");
-    evhtp_set_cb(htp, "/1/ping.json", testcb, "two");
-    evhtp_set_cb(htp, "/issue161", issue161cb, NULL);
+    evhtp_set_cb(htp, "/", handler_greeting, NULL);
+    evhtp_set_regex_cb(htp, "^/[0-9]+$", handler_main, NULL);
+    evhtp_set_regex_cb(htp, "^/[0-9]+/[A-Za-z0-9]+$", handler_set, NULL);
+    evhtp_set_cb(htp, "/shutdown", handler_shutdown, NULL);
+    evhtp_set_regex_cb(htp, "^/memsize/[0-9]+$", handler_setup, NULL);
+    
     #ifndef EVHTP_DISABLE_EVTHR
     evhtp_use_threads(htp, NULL, 8, NULL);
     #endif
